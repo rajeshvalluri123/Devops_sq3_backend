b0b4b64308d464b4bbf5aa8175310ada1168939e
rajeshvalluri123 <70281671+rajeshvalluri123@users.noreply.github.com>
2024-12-17 21:55:47 +0530

    Update StocktoshipApplicationTests.java

--- src/test/java/com/bosch/stocktoship/StocktoshipApplicationTests.java
+++ src/test/java/com/bosch/stocktoship/StocktoshipApplicationTests.java
@@ -2,14 +2,32 @@ package com.bosch.stocktoship;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)  // Replace @RunWith with @ExtendWith for JUnit 5
 @SpringBootTest
-@RunWith(SpringRunner.class) 
 class StocktoshipApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
+
+
+// package com.bosch.stocktoship;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// @RunWith(SpringRunner.class) 
+// class StocktoshipApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
 
